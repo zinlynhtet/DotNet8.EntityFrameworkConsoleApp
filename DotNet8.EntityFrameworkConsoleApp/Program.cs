b829238102724fe70edaa97fb645bb307774bfd5
Zin Linn Htet <mackk5504@gmail.com>
2024-04-24 23:27:21 +0630

    -

--- DotNet8.EntityFrameworkConsoleApp/Program.cs
+++ DotNet8.EntityFrameworkConsoleApp/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DotNet8.EntityFrameworkConsoleApp.DbService.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection()
+    .AddDbContext<AppDbContext>()
+    .BuildServiceProvider();
+var EFCoreService = services.GetRequiredService<AppDbContext>();
+EFCoreService.Run();
