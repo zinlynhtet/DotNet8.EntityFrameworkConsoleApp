8f17a28f93bcd7a2462a76fb51cfb51ad303c78b
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 11:48:47 +0630

    -

---
+++ DotNet8.EntityFrameworkConsoleApp/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+namespace DotNet8.EntityFrameworkConsoleApp.Models
+{
+    public class AppDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+                {
+
+                    DataSource = ".", // sever name
+                    InitialCatalog = "TestDb", //database name
+                    UserID = "sa",
+                    Password = "sasa@123",
+                    TrustServerCertificate = true
+                };
+                optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+            }
+        }
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
