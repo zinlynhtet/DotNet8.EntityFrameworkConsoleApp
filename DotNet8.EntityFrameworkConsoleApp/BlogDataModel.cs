f22e2a5e361e040acf72a0abbea54fb1d5eb3342
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 11:25:18 +0630

    Add Data Model

---
+++ DotNet8.EntityFrameworkConsoleApp/BlogDataModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.EntityFrameworkConsoleApp
+{
+    [Table("Tbl_Blogs")]
+    public class BlogDataModel
+    {
+        [Key]
+        [Column("BlogId")]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+}
