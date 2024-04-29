bc99538eaec004af7e7ea6c6e827a91f0d1aa731
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 10:36:56 +0630

    Add Connection

---
+++ DotNet8.EntityFrameworkConsoleApp.DbService/Models/Connection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+namespace DotNet8.EntityFrameworkConsoleApp.DbService.Models
+{
+    public class Connection
+    {
+        public string ConnectionString()
+        {
+            var sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = ".",
+                InitialCatalog = "TestDb",
+                UserID = "sa",
+                Password = "sasa@123",
+                TrustServerCertificate = true
+            };
+            return sqlConnectionStringBuilder.ConnectionString;
+        }
+    }
+}
