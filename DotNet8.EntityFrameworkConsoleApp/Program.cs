5ba0c06126ab135bc7999a4b8ff74ac0041e5204
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 10:37:30 +0630

    -

--- DotNet8.EntityFrameworkConsoleApp/Program.cs
+++ DotNet8.EntityFrameworkConsoleApp/Program.cs
@@ -1,8 +1,11 @@
-﻿using DotNet8.EntityFrameworkConsoleApp.DbService.Models;
+﻿using DotNet8.EntityFrameworkConsoleApp;
+using DotNet8.EntityFrameworkConsoleApp.DbService.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection()
+    .AddScoped<Connection>()
     .AddDbContext<AppDbContext>()
+    .AddScoped<EFCoreService>()
     .BuildServiceProvider();
-var EFCoreService = services.GetRequiredService<AppDbContext>();
-EFCoreService.Run();
+EFCoreService efCoreService = services.GetRequiredService<EFCoreService>();
+efCoreService.Run();
