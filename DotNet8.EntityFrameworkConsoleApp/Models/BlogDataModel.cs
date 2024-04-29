25664235fe0b49a41639b60fd99d7a212f1fabda
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 13:50:38 +0630

    Complete Blog Data Model

--- DotNet8.EntityFrameworkConsoleApp/Models/BlogDataModel.cs
+++ DotNet8.EntityFrameworkConsoleApp/Models/BlogDataModel.cs
@@ -1,17 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿namespace DotNet8.EntityFrameworkConsoleApp.Models;
 
-
-namespace DotNet8.EntityFrameworkConsoleApp.Models
+[Table("Tbl_Blogs")]
+public class BlogDataModel
 {
-    [Table("Tbl_Blogs")]
-    public class BlogDataModel
-    {
-        [Key]
-        [Column("BlogId")]
-        public int BlogId { get; set; }
-        public string? BlogTitle { get; set; }
-        public string? BlogAuthor { get; set; }
-        public string? BlogContent { get; set; }
-    }
+    [Key]
+    [Column("BlogId")]
+    public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
 }
