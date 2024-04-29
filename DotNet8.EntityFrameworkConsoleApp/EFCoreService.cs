0c821b3272adea11b2ffbc5a5e0e13b7e86a7090
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 10:37:25 +0630

    Add efcore service

--- DotNet8.EntityFrameworkConsoleApp/EFCoreService.cs
+++ DotNet8.EntityFrameworkConsoleApp/EFCoreService.cs
@@ -1,6 +1,26 @@
-﻿namespace DotNet8.EntityFrameworkConsoleApp;
+﻿using DotNet8.EntityFrameworkConsoleApp.DbService.Models;
+
+namespace DotNet8.EntityFrameworkConsoleApp;
 
 public class EFCoreService
 {
+    private readonly AppDbContext _context;
+    public EFCoreService(AppDbContext context)
+    {
+        _context = context;
+    }
 
+    public void Run()
+    {
+        Read();
+    }
+
+    private void Read()
+    {
+        List<TblBlog> lst = _context.TblBlogs.ToList();
+        foreach (TblBlog item in lst)
+        {
+            item.ToConsole();
+        }
+    }
 }
