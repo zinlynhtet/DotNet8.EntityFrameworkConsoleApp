9f4ec2b405b953bb47d78f7ea33e1a0853fd301f
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 13:51:21 +0630

    Complete Global Using

---
+++ DotNet8.EntityFrameworkConsoleApp/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// Global using directives
+
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using DotNet8.EntityFrameworkConsoleApp.Models;
+global using DotNet8.EntityFrameworkConsoleApp.Services;
+global using Microsoft.Data.SqlClient;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
