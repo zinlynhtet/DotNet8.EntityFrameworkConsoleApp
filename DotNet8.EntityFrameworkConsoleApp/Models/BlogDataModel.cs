de5bd16ec5c2a9bfe2f35675a159fbe0be9d816c
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 11:48:50 +0630

    -

---
+++ DotNet8.EntityFrameworkConsoleApp/Models/BlogDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace DotNet8.EntityFrameworkConsoleApp.Models
+{
+    [Table("Tbl_Blogs")]
+    public class BlogDataModel
+    {
+        [Key]
+        [Column("BlogId")]
+        public int BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+    }
+}
