c6c405598d2cee9f95ff97395c94ce08476b9316
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 11:57:20 +0630

    Comple Create Edit and Read List

--- DotNet8.EntityFrameworkConsoleApp/Services/EFCoreService.cs
+++ DotNet8.EntityFrameworkConsoleApp/Services/EFCoreService.cs
@@ -1,4 +1,5 @@
 ﻿using DotNet8.EntityFrameworkConsoleApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNet8.EntityFrameworkConsoleApp.Services;
 
@@ -14,6 +15,7 @@ public class EFCoreService
     public void Run()
     {
         Read();
+        //Create();
     }
 
     private void Read()
@@ -23,5 +25,45 @@ public class EFCoreService
         {
             item.ToConsole();
         }
+    }
+    private void Edit(int id)
+    {
+        BlogDataModel? item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            Console.WriteLine("No data found.");
+            return;
+        }
+        item.ToConsole();
+    }
+
+    #region user input
+    private static string? UserInput(out string? author, out string? content)
+    {
+        Console.Write("Enter blog title: ");
+        string title = Console.ReadLine();
+
+        Console.Write("Enter blog author: ");
+        author = Console.ReadLine();
+
+        Console.Write("Enter blog content: ");
+        content = Console.ReadLine();
+        return title;
+    }
+
+    #endregion
+    private void Create()
+    {
+        var title = UserInput(out var author, out var content);
+        BlogDataModel model = new BlogDataModel
+        {
+            BlogTitle = title,
+            BlogAuthor = author,
+            BlogContent = content
+        };
+        _context.Blogs.Add(model);
+        int result = _context.SaveChanges();
+        string message = result > 0 ? "Saving Successful." : "Saving Failed.";
+        message.ToConsoleMessage();
     }
 }
