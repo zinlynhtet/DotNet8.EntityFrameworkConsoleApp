c1071027307b01de65185debfd38d3f9fbbf35ac
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 11:25:08 +0630

    Add Db Context

---
+++ DotNet8.EntityFrameworkConsoleApp/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.EntityFrameworkConsoleApp
+{
+    public class AppDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+                {
+
+                    DataSource = ".", // sever name
+                    InitialCatalog = "TestDb", //database name
+                    UserID = "sa",
+                    Password = "sasa@123",
+                    TrustServerCertificate = true
+                };
+                optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+            }
+        }
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
