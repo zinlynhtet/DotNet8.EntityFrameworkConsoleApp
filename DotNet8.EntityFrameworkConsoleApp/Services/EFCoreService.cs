437586a6cbd34188c378c74f67e6fc609ccc1eaf
Zin Linn Htet <mackk5504@gmail.com>
2024-04-29 11:48:58 +0630

    -

---
+++ DotNet8.EntityFrameworkConsoleApp/Services/EFCoreService.cs
@@ -0,0 +1,27 @@
+﻿using DotNet8.EntityFrameworkConsoleApp.Models;
+
+namespace DotNet8.EntityFrameworkConsoleApp.Services;
+
+public class EFCoreService
+{
+    private readonly AppDBContext _context;
+
+    public EFCoreService(AppDBContext context)
+    {
+        _context = new AppDBContext();
+    }
+
+    public void Run()
+    {
+        Read();
+    }
+
+    private void Read()
+    {
+        List<BlogDataModel> lst = _context.Blogs.ToList();
+        foreach (BlogDataModel item in lst)
+        {
+            item.ToConsole();
+        }
+    }
+}
